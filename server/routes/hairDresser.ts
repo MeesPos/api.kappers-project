8b60e672355cfb921206cc06c36a970bbb792182
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-11-22 07:50:51 +0100

    init via typescript-express-starter

--- server/routes/hairDresser.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express';
-var router = Router();
-
-
-router.get('/', function(req: Request, res: Response, next) {
-  res.send('Marjolein knip knip');
-});
-
-export default router;
