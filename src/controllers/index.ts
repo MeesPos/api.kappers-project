b7b44321f6f0897033211b9f4e3f646343ef73a8
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-11-22 14:37:40 +0100

    this works??

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,27 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express'
+
+import mysql from 'mysql'
 
 const getIndex = async (req: Request, res: Response, next: NextFunction) => {
-	return res.status(200).json({
-		hoi: true,
-	});
-};
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'dbuser',
+        password: '',
+        database: 'my_db',
+    })
 
-export default { getIndex };
+    connection.connect()
+
+    const sql = connection.query(
+        'SELECT 1 + 1 AS solution',
+        (err, rows, fields) => {
+            if (err) throw err
+
+            res.send('The solution is: ' + rows[0].solution)
+        }
+    )
+
+    connection.end()
+}
+
+export default { getIndex }
