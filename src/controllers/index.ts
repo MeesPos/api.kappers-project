824f5eb498f4f530bae73ab43cb6e0102da95401
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-11-22 14:41:06 +0100

    clean up

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -5,21 +5,18 @@ import mysql from 'mysql'
 const getIndex = async (req: Request, res: Response, next: NextFunction) => {
     const connection = mysql.createConnection({
         host: 'localhost',
-        user: 'dbuser',
+        user: 'test',
         password: '',
         database: 'my_db',
     })
 
     connection.connect()
 
-    const sql = connection.query(
-        'SELECT 1 + 1 AS solution',
-        (err, rows, fields) => {
-            if (err) throw err
+    connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+        if (err) throw err
 
-            res.send('The solution is: ' + rows[0].solution)
-        }
-    )
+        res.send('The solution is: ' + rows[0].solution)
+    })
 
     connection.end()
 }
