da0d20fe3f46ff8142b94ccdfd5d20892277c5c6
MeesPos <meespostma_@hotmail.com>
2022-11-28 10:30:32 +0100

    Added working authentication

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -4,13 +4,26 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
+    // req.body = JSON.parse(req.body);
     const { email, password } = req.body;
 
     let existingUser = await prisma.hairdresser.findFirst({
         where: {
-            
+            email: email
         }
     })
 
-    res.json(req.body);
+    console.log(existingUser, password, email);
+
+    if (existingUser === null || existingUser.password != password) {
+        Promise.resolve().then(() => {
+            throw new Error('invalid credentials');
+        }).catch(next);
+
+        res.status(401);
+        
+        return;
+    }
+
+    res.json(existingUser);
 }
