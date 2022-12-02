b4630616c428e575f4cd4d506c4df72885afbf21
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-12-02 16:29:59 +0100

    cleanup vars/if statements

--- src/controllers/auth/forgotPassword.ts
+++ src/controllers/auth/forgotPassword.ts
@@ -1,99 +1,105 @@
-import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-import { transporter } from "../../utils/mailer";
-import jwt from 'jsonwebtoken';
+import { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import { transporter } from '../../utils/mailer'
+import jwt from 'jsonwebtoken'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export const sendMail = async (
-    req: Request,
-    res: Response
-) => {
-    const { email } = req.body;
+export const sendMail = async (req: Request, res: Response) => {
+    const { email } = req.body
 
-    let existingUser = await prisma.hairdresser.findFirst({
+    const existingUser = await prisma.hairdresser.findFirst({
         where: {
-            email: email
-        }
-    });
+            email: email,
+        },
+    })
 
-    if (existingUser === null) {
+    if (!existingUser) {
         /*
         We do not want to indicate that the user does not exist, this is a security issue that we must avoid.
 
         SOURCE - https://cheatsheetseries.owasp.org/cheatsheets/Forgot_Password_Cheat_Sheet.html
         */
-        res.status(200).send('Email sent');
+        res.status(200).send('Email sent')
 
-        return;
+        return
     }
 
-    const jwt_token = jwt.sign({
-        email: existingUser.email,
-        name: existingUser.name
-    }, process.env.JWT_SECRET!, {
-        expiresIn: 12 * 60 * 60
-    });
+    const jwt_token = jwt.sign(
+        {
+            email: existingUser.email,
+            name: existingUser.name,
+        },
+        process.env.JWT_SECRET!,
+        {
+            expiresIn: 12 * 60 * 60,
+        }
+    )
 
-    const url = process.env.FRONTEND_URL + '/reset-password/' + jwt_token + '/' + existingUser.email;
+    const url =
+        process.env.FRONTEND_URL +
+        '/reset-password/' +
+        jwt_token +
+        '/' +
+        existingUser.email
 
-    transporter.sendMail({
-        from: process.env.MAIL_FROM,
-        to: existingUser.email,
-        subject: "Password reset",
-        html: `Hello, <br><br>Reset your password to click on this button:<br> <a href="${url}"><button>Reset wachtwoord</button></a><br><br>Or click on this URL:<br><a href="${url}">${url}</a>`
-    }, () => {
-        res.status(200).send('Email sent');
-    })
+    transporter.sendMail(
+        {
+            from: process.env.MAIL_FROM,
+            to: existingUser.email,
+            subject: 'Password reset',
+            html: `Hello, <br><br>Reset your password to click on this button:<br> <a href="${url}"><button>Reset wachtwoord</button></a><br><br>Or click on this URL:<br><a href="${url}">${url}</a>`,
+        },
+        () => {
+            res.status(200).send('Email sent')
+        }
+    )
 }
 
-export const resetPassword = async (
-    req: Request,
-    res: Response
-) => {
-    const { email, password, token } = req.body;
+export const resetPassword = async (req: Request, res: Response) => {
+    const { email, password, token } = req.body
 
-    let existingUser = await prisma.hairdresser.findFirst({
+    const existingUser = await prisma.hairdresser.findFirst({
         where: {
-            email: email
-        }
-    });
+            email: email,
+        },
+    })
 
-    if (existingUser === null) {
+    if (!existingUser) {
         /*
         We do not want to indicate that the user does not exist, this is a security issue that we must avoid.
 
         SOURCE - https://cheatsheetseries.owasp.org/cheatsheets/Forgot_Password_Cheat_Sheet.html
         */
-        res.status(498).send('Token is invalid');
+        res.status(498).send('Token is invalid')
 
-        return;
+        return
     }
 
     try {
         // @ts-ignore
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         // @ts-ignore
         if (decoded?.email !== existingUser?.email) {
-            res.status(498).send('Token is invalid');
+            res.status(498).send('Token is invalid')
 
-            return;
+            return
         }
 
         const updatedUser = await prisma.hairdresser.update({
             where: {
-                email: existingUser.email
+                email: existingUser.email,
             },
             data: {
-                password: password
-            }
+                password: password,
+            },
         })
 
-        res.send('Password updated');
+        res.send('Password updated')
     } catch (err) {
-        res.status(498).send('Token is invalid');
+        res.status(498).send('Token is invalid')
 
-        return;
+        return
     }
 }
--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -1,44 +1,50 @@
-import { Request, Response, NextFunction } from "express";
-import { PrismaClient } from "@prisma/client";
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express'
+import { PrismaClient } from '@prisma/client'
+import jwt from 'jsonwebtoken'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export const login = async (req: Request, res: Response, next: NextFunction) => {
-    const { email, password } = req.body;
+export const login = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { email, password } = req.body
 
-    let existingUser = await prisma.hairdresser.findFirst({
+    const existingUser = await prisma.hairdresser.findFirst({
         where: {
-            email: email
-        }
+            email: email,
+        },
     })
 
-    if (existingUser === null || existingUser.password != password) {
-        Promise.resolve().then(() => {
-            throw new Error('invalid credentials');
-        }).catch(next);
+    if (!existingUser || existingUser.password != password) {
+        Promise.resolve()
+            .then(() => {
+                throw new Error('invalid credentials')
+            })
+            .catch(next)
 
-        res.status(401);
-        
-        return;
+        res.status(401)
+
+        return
     }
 
     const user = {
         email: existingUser.email,
-        name: existingUser.name
+        name: existingUser.name,
     }
 
     const accessToken = jwt.sign(user, process.env.JWT_SECRET!, {
-        expiresIn: 12 * 60 * 60
-    });
+        expiresIn: 12 * 60 * 60,
+    })
 
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET!, {
-        expiresIn: 12 * 60 * 60
-    });
+        expiresIn: 12 * 60 * 60,
+    })
 
     res.json({
         user: user,
         accessToken: accessToken,
-        refreshToken: refreshToken
-    });
+        refreshToken: refreshToken,
+    })
 }
